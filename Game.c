7d0ffc6cfc89f85002fc2eda5cdf774f582c3ad6
Decease <Romab1cool1@mail.ru>
2024-02-20 23:31:20 +0700

    Изменил названия файлов и добавил описания всяких приколов, чекайте папки

---
+++ Game.c
@@ -0,0 +1,23 @@
+#include "Game_data/FilesData.h"
+//тут был староста
+//тут была костыльная рожа
+//Тут был нормальный пацан...
+//тут было халявное камео
+
+int main(void)
+{   
+    begin=time(NULL);
+    EnemysArray =malloc(EnemysCount*sizeof(struct enemy));
+    PreLoader();              // Догружаем микропроцессы присваивания классов и т.д. читайте прато
+    srand(time(NULL));
+    system("clear");
+    printf("Версия 32\n");
+    struct player conecntion;        // Создаём объект класса нашего пользователя, в котором всё будет храниться. Позже мы будем этот объект сохранять и вызывать в будущем
+    createHero(&conecntion);        // Передаём разименнованный объект в функции с скриптами создания персоонажа
+    while (conecntion.player_alive){
+        gameSelectMenu(&conecntion);
+    }
+    EndGame(begin);
+    printf("Вы накопили: %d очков\n",conecntion.playerScore);
+    return 0;
+}
